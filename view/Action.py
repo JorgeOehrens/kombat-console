d13d3dced37020facd7c856aa65d0bcbc2502ccb
Jorge Oehrens <jorge.oehrens@gmail.com>
2022-09-12 18:06:36 -0300

    Preparing fighting actions

---
+++ view/Action.py
@@ -0,0 +1,94 @@
+from enum import Enum
+
+
+class Action(Enum):
+    ARRIBA= "W"
+    ABAJO= "S"
+    IZQUIERDA= "A"
+    DERECHA= "D"
+    PUÑO= "P"
+    PATADA= "K"
+    TALADOKEN= "DSDP"
+    TALADOKEN2= "ASAP"
+    REMUYUKEN= "DSDP"
+    REMUYUKEN2= "SAK"
+
+    def description(self) -> str:
+        if self == self.ARRIBA:
+            return "Arriba"
+        elif self == self.ABAJO:
+            return "Abajo"
+        elif self ==  self.IZQUIERDA:
+            return "Izquierda"
+        elif self == self.DERECHA:
+            return "Derecha"
+        elif self == self.PUÑO:
+            return "Puño"
+        elif self == self.PATADA:
+            return "Patada"
+        elif self == self.TALADOKEN:
+            return "Taladoken"
+        elif self == self.TALADOKEN2:
+            return "Taladoken"
+        elif self == self.REMUYUKEN:
+            return "Remuyuken"
+        elif self == self.REMUYUKEN2:
+            return "REMUYUKEN"
+        else:
+            raise Exception("Movimiento no valido")
+
+    def __str__ (self):
+        if self == self.ARRIBA:
+            return "Arriba"
+        elif self == self.ABAJO:
+            return "Abajo"
+        elif self ==  self.IZQUIERDA:
+            return "Izquierda"
+        elif self == self.DERECHA:
+            return "Derecha"
+        elif self == self.PUÑO:
+            return "Puño"
+        elif self == self.PATADA:
+            return "Patada"
+        elif self == self.TALADOKEN:
+            return "Taladoken"
+        elif self == self.TALADOKEN2:
+            return "Taladoken"
+        elif self == self.REMUYUKEN:
+            return "Remuyuken"
+        elif self == self.REMUYUKEN2:
+            return "REMUYUKEN"
+        else:
+            raise Exception("Movimiento no valido")
+
+    @classmethod
+
+    def strtToAction(cls,string) -> 'Action':
+        if string == "W":
+            return cls.ARRIBA
+        elif string == "S":
+            return cls.ABAJO
+        elif string == "A":
+            return cls.IZQUIERDA
+        elif string =="D":
+            return cls.DERECHA
+        elif string == "P":
+            return cls.PUÑO
+        elif string == "K":
+            return cls.PATADA
+        elif string == "DSDP":
+            return cls.TALADOKEN
+        elif string == "ASP":
+            return cls.TALADOKEN2
+        elif string == "DSDP":
+            return cls.TALADOKN2
+        elif string == "SAK":
+            return cls.TALADOKEN2
+
+
+
+
+
+
+
+
--- view/Combat.py
+++ view/Combat.py
@@ -1,16 +1,27 @@
+from models.characters.Character import Character
 
+from controller.Game import Game
 
 class Combat:
-
     def __init__(self, game: 'Game',player1: 'Character', player2: 'Character'):
-
         self.game= game
         self.player1=player1
         self.player2 = player2
-
         self.fight()
 
-        def fight(self) -> None:
-            
+    def fight(self) -> None:
+        while self.game.ifLiveCharacter():
+            print(' {} esta atacando'.format(self.player1))
+
+
+    def turnoJugador(self) -> None:
+
+        print(self.jugador.__str__())
+        self.optionsActions()
+
+    def optionsActions(self):
+
+
+
 
 
